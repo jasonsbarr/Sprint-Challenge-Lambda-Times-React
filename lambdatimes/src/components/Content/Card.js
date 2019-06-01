8f05586e986a279ea8f2b910ed1a5b421dd51d7f
Jason Barr <jason@jasonsbarr.com>
2019-06-01 13:06:08 -0500

    Add PropTypes to Card

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Card = props => {
   return (
@@ -6,7 +7,7 @@ const Card = props => {
       <div className="headline">{/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={"" /* image source goes here */} />
         </div>
         <span>By {/* author goes here */}</span>
       </div>
@@ -15,5 +16,8 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default Card;
