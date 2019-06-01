906acf5518d399ef4d9e0f77fe129c03611d18aa
Jason Barr <jason@jasonsbarr.com>
2019-06-01 12:59:46 -0500

    Define PropTypes for Cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
+import PropTypes from "prop-types";
 
 const Cards = props => {
   return (
@@ -7,9 +8,12 @@ const Cards = props => {
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+};
 
 export default Cards;
