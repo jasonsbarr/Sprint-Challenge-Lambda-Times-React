e29bc1c97867bba084655db0b66d17c45b6f0d5b
Jason Barr <jason@jasonsbarr.com>
2019-06-01 12:50:26 -0500

    Set state when new tab is selected

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,6 +26,7 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -45,6 +46,7 @@ export default class Content extends Component {
   };
 
   render() {
+    const { tabs, selected } = this.state;
     return (
       <div className="content-container">
         {/* 
@@ -52,7 +54,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          selectTabHandler={this.changeSelected}
+          tabs={tabs}
+          selectedTab={selected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
